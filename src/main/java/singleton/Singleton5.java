25c352f7cc032c952368406a6454b070aa7c99ec
songmk <stalker5217@gmail.com>
2022-07-02 23:15:35 +0900

    Singleton pattern

---
+++ src/main/java/singleton/Singleton5.java
@@ -0,0 +1,13 @@
+package singleton;
+
+public class Singleton5 {
+	private Singleton5() {}
+
+	private static class SingletonHolder {
+		private static final Singleton5 INSTANCE = new Singleton5();
+	}
+
+	public static Singleton5 getInstance() {
+		return SingletonHolder.INSTANCE;
+	}
+}
---
+++ src/main/java/singleton/Singleton6.java
@@ -0,0 +1,5 @@
+package singleton;
+
+public enum Singleton6 {
+	INSTANCE;
+};
