b77ac351d474bb7960b0c1188fc35cb11d3ca4d3
songmk <stalker5217@gmail.com>
2022-09-11 23:56:20 +0900

    factory method pattern

---
+++ src/main/java/factory_method/PizzaStore.java
@@ -0,0 +1,15 @@
+package factory_method;
+
+public interface PizzaStore {
+	Pizza createPizza(String item);
+
+	default Pizza orderPizza(String type) {
+		Pizza pizza = createPizza(type);
+		System.out.println("--- Making a " + pizza.getName() + " ---");
+		pizza.prepare();
+		pizza.bake();
+		pizza.cut();
+		pizza.box();
+		return pizza;
+	}
+}
